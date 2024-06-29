7f0340437991b96774a91ac225e27f311bd79ef4
Rio-iOS <rio.2014.swift.ios@gmail.com>
2024-06-29 17:42:18 +0900

    feat: create new password-reset project

---
+++ Password-Reset/Password-Reset/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Password-Reset
+//
+//  Created by 藤門莉生 on 2024/06/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
